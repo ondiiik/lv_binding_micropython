aac5f15713669177d465e2d9ef345210ae831c32
OSi (Ondrej Sienczak) <ondrej.sienczak@seznam.cz>
2021-08-25 11:14:15 +0200

    updated

--- lv_conf.h
+++ lv_conf.h
@@ -595,7 +595,9 @@ typedef void * lv_obj_user_data_t;
 /*Calendar (dependencies: -)*/
 #define LV_USE_CALENDAR 1
 #if LV_USE_CALENDAR
-#  define LV_CALENDAR_WEEK_STARTS_MONDAY    0
+#  ifndef LV_CALENDAR_WEEK_STARTS_MONDAY
+#    define LV_CALENDAR_WEEK_STARTS_MONDAY    0
+#  endif
 #endif
 
 /*Canvas (dependencies: lv_img)*/
